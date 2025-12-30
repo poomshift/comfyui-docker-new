397695ca51a6c18b625acfbabe2fa7330f2ace82
ภัทรภูมิ เติงจันต๊ะ <poomshift@gmail.com>
2025-12-30 18:09:32 +0700

    Add ComfyUI-FlashVSR_Stable custom node

--- start.sh
+++ start.sh
@@ -246,6 +246,7 @@ if [ ! -e "/workspace/ComfyUI/main.py" ]; then
     git clone --depth=1 https://github.com/kijai/ComfyUI-MelBandRoFormer.git 2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-MelBandRoFormer | tee -a /workspace/logs/comfyui.log
     git clone --depth=1 https://github.com/kijai/ComfyUI-segment-anything-2.git  2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-segment-anything-2 | tee -a /workspace/logs/comfyui.log
     git clone --depth=1 https://github.com/kijai/ComfyUI-WanAnimatePreprocess.git  2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-WanAnimatePreprocess | tee -a /workspace/logs/comfyui.log
+    git clone --depth=1 https://github.com/naxci1/ComfyUI-FlashVSR_Stable.git 2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-FlashVSR_Stable | tee -a /workspace/logs/comfyui.log
     
     echo "Total size of custom nodes:" | tee -a /workspace/logs/comfyui.log && du -sh . | tee -a /workspace/logs/comfyui.log
 
