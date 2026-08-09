7a08b9157591c563538d3b740dd920084376c945
promptalchemist <130088861+poomshift@users.noreply.github.com>
2026-08-09 14:44:08 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -258,6 +258,8 @@ if [ ! -e "/workspace/ComfyUI/main.py" ]; then
     git clone --depth=1 https://github.com/MadiatorLabs/ComfyUI-RunpodDirect.git 2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-RunpodDirect | tee -a /workspace/logs/comfyui.log
     git clone --depth=1 https://github.com/evanspearman/ComfyMath.git 2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyMath | tee -a /workspace/logs/comfyui.log
     git clone --depth=1 https://github.com/richservo/rs-nodes.git 2>&1 | tee -a /workspace/logs/comfyui.log && du -sh rs-nodes | tee -a /workspace/logs/comfyui.log
+    git clone --depth=1 https://github.com/xmarre/ComfyUI-Spectrum-MiniMax-H3.git  2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-Spectrum-MiniMax-H3 | tee -a /workspace/logs/comfyui.log
+    git clone --depth=1 https://github.com/kijai/ComfyUI-SolAttn_triton.git  2>&1 | tee -a /workspace/logs/comfyui.log && du -sh ComfyUI-SolAttn_triton | tee -a /workspace/logs/comfyui.log
     echo "Total size of custom nodes:" | tee -a /workspace/logs/comfyui.log && du -sh . | tee -a /workspace/logs/comfyui.log 
 
     # Install custom nodes requirements
